51106e77af8322d727e2053f15dfae91ae6a89bf
dylan <dylancabral.93@gmail.com>
2022-12-19 23:55:42 -0800

    updating lab 1 in progress, accidently deactivated venv

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,1 +1,94 @@
-print("hello world")
+from textwrap import dedent
+def hello_world():
+    print("Hello World!")
+
+def menu_display():
+    """
+    menu function!
+    """
+    print(dedent("""**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************"""))
+    your_order = input("> ")
+    print(f"{your_order} was added to the bill!")
+
+    ask_again = True
+    while ask_again:
+        choice = input("Are You Still Hungry?")
+        if choice == "y:":
+            print(dedent("""**************************************
+                     **    Welcome to the Snakes Cafe!   **
+                     **    Please see our menu below.    **
+                     **
+                     ** To quit at any time, type "quit" **
+                     **************************************
+
+                     Appetizers
+                     ----------
+                     Wings
+                     Cookies
+                     Spring Rolls
+
+                     Entrees
+                     -------
+                     Salmon
+                     Steak
+                     Meat Tornado
+                     A Literal Garden
+
+                     Desserts
+                     --------
+                     Ice Cream
+                     Cake
+                     Pie
+
+                     Drinks
+                     ------
+                     Coffee
+                     Tea
+                     Unicorn Tears
+
+                     ***********************************
+                     ** What would you like to order? **
+                     ***********************************"""))
+            your_order = input("> ")
+            print(f"{your_order} was added to the bill!")
+        else:
+            break
+
+
+if __name__ == "__main__":
+    hello_world()
+    menu_display()
